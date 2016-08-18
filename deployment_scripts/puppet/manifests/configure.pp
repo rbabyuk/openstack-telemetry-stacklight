552a4788d17e76f800689e934f2368d1a9905e70
Roman Babyuk <rbabyuk@mirantis.com>
2016-08-18 14:51:40 +0300

    adjusting ceilometer config

--- deployment_scripts/puppet/manifests/configure.pp
+++ deployment_scripts/puppet/manifests/configure.pp
@@ -27,18 +27,35 @@ $elasticsearch_port   = hiera('lma::collector::elasticsearch::rest_port')
 $influxdb_vip         = $aodh_nodes["$::hostname"]["network_roles"]["influxdb_vip"]
 $influxdb_port        = hiera('lma::collector::influxdb::port')
 $influx_database      = hiera('lma::collector::influxdb::database')
-$influx_user          = hiera('lma::collector::influxdb::user')
+$influx_user          = 'root' # hiera('lma::collector::influxdb::user')
 $influx_password      = hiera('lma::collector::influxdb::password')
 $influx_root_password = hiera('lma::collector::influxdb::root_password')
+
+# Let's use alreadt defined params for ceilometer
+include ::ceilometer::params
+$ceilometer_service_name = $::ceilometer::params::api_service_name
 
 # calculated values
 $metering_connection = "stacklight://${influx_user}:${influx_password}@${influxdb_vip}:${influxdb_port}/ceilometer"
 $resource_connection = "es://${elasticsearch_node}:${elasticsearch_port}"
 $event_connection    = "es://${elasticsearch_node}:${elasticsearch_port}"
+$connection = $metering_connection
 
 # TODO validate values before proceed
 
 ceilometer_config { 'database/metering_connection': value => "${metering_connection}" }
 ceilometer_config { 'database/resource_connection': value => "${resource_connection}" }
 ceilometer_config { 'database/event_connection': value => "${event_connection}" }
+ceilometer_config { 'database/connection': value => "${connection}" }
+
+service { 'ceilometer-service':
+      ensure     => $service_ensure,
+      name       => $::ceilometer::params::api_service_name,
+      enable     => $enabled,
+      hasstatus  => true,
+      hasrestart => true,
+      tag        => 'ceilometer-service',
+    }
+
+Ceilometer_config<||> ~> Service['ceilometer-service']
 
